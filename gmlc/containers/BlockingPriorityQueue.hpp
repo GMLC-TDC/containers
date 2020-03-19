a7e8f4ac3297a0b0a76b659666c0490933070645
Philip Top <phlptp@gmail.com>
2020-03-18 17:18:34 -0700

    update formatting and copyright notice

--- gmlc/containers/BlockingPriorityQueue.hpp
+++ gmlc/containers/BlockingPriorityQueue.hpp
@@ -1,8 +1,10 @@
 /*
-Copyright (c) 2017-2019,
+Copyright (c) 2017-2020,
 Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance
 for Sustainable Energy, LLC.  See the top-level NOTICE for additional details.
-All rights reserved. SPDX-License-Identifier: BSD-3-Clause
+All rights reserved. 
+
+SPDX-License-Identifier: BSD-3-Clause
 */
 #pragma once
 
