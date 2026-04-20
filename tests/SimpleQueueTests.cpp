81724fbad43181700a3f22be991ae5851953105c
Philip Top <phlptp@gmail.com>
2026-04-20 06:35:46 -0700

    update tests for clang-tidy

--- tests/SimpleQueueTests.cpp
+++ tests/SimpleQueueTests.cpp
@@ -5,11 +5,14 @@ for Sustainable Energy, LLC.  See the top-level NOTICE for additional details.
 All rights reserved. SPDX-License-Identifier: BSD-3-Clause
 */
 #include "gtest/gtest.h"
-#include <cstdio>
+#include <chrono>
+#include <cstdint>
 #include <future>
+#include <iostream>
 #include <memory>
 #include <thread>
 #include <utility>
+#include <vector>
 /** these test cases test data_block and data_view objects
  */
 
@@ -19,179 +22,173 @@ using gmlc::containers::SimpleQueue;
 /** test basic operations */
 TEST(simple_queue_tests, basic_tests)
 {
-    SimpleQueue<int> sq;
+    SimpleQueue<int> queue;
 
-    sq.push(45);
-    sq.push(54);
+    queue.push(45);
+    queue.push(54);
 
-    EXPECT_TRUE(sq.empty() == false);
+    EXPECT_FALSE(queue.empty());
 
-    EXPECT_EQ(sq.size(), 2);
-    auto b = sq.pop();
-    EXPECT_EQ(*b, 45);
-    b = sq.pop();
-    EXPECT_EQ(*b, 54);
+    EXPECT_EQ(queue.size(), 2);
+    auto popped_value = queue.pop();
+    EXPECT_EQ(*popped_value, 45);
+    popped_value = queue.pop();
+    EXPECT_EQ(*popped_value, 54);
 
-    b = sq.pop();
-    EXPECT_TRUE(!(b));
-    EXPECT_TRUE(sq.empty());
+    popped_value = queue.pop();
+    EXPECT_FALSE(popped_value);
+    EXPECT_TRUE(queue.empty());
 }
 
 /** test with a move only element*/
 TEST(simple_queue_tests, move_only_tests)
 {
-    SimpleQueue<std::unique_ptr<double>> sq;
+    SimpleQueue<std::unique_ptr<double>> queue;
 
-    sq.push(std::make_unique<double>(4534.23));
+    queue.push(std::make_unique<double>(4534.23));
 
-    auto e2 = std::make_unique<double>(34.234);
-    sq.push(std::move(e2));
+    auto second_element = std::make_unique<double>(34.234);
+    queue.push(std::move(second_element));
 
-    EXPECT_TRUE(sq.empty() == false);
+    EXPECT_FALSE(queue.empty());
 
-    EXPECT_EQ(sq.size(), 2);
-    auto b = sq.pop();
-    EXPECT_EQ(**b, 4534.23);
-    b = sq.pop();
-    EXPECT_EQ(**b, 34.234);
+    EXPECT_EQ(queue.size(), 2);
+    auto popped_value = queue.pop();
+    EXPECT_EQ(**popped_value, 4534.23);
+    popped_value = queue.pop();
+    EXPECT_EQ(**popped_value, 34.234);
 
-    b = sq.pop();
-    EXPECT_TRUE(!(b));
-    EXPECT_TRUE(sq.empty());
+    popped_value = queue.pop();
+    EXPECT_FALSE(popped_value);
+    EXPECT_TRUE(queue.empty());
 }
 
 /** test the ordering with a larger number of inputs*/
-
 TEST(simple_queue_tests, ordering_tests)
 {
-    SimpleQueue<int> sq;
+    SimpleQueue<int> queue;
 
-    for (int ii = 1; ii < 10; ++ii) {
-        sq.push(ii);
-    }
-
-    auto b = sq.pop();
-    EXPECT_EQ(*b, 1);
-    for (int ii = 2; ii < 7; ++ii) {
-        b = sq.pop();
-        EXPECT_EQ(*b, ii);
-    }
-    for (int ii = 10; ii < 20; ++ii) {
-        sq.push(ii);
-    }
-    for (int ii = 7; ii < 20; ++ii) {
-        b = sq.pop();
-        EXPECT_EQ(*b, ii);
+    for (int index = 1; index < 10; ++index) {
+        queue.push(index);
     }
 
-    EXPECT_TRUE(sq.empty());
+    auto popped_value = queue.pop();
+    EXPECT_EQ(*popped_value, 1);
+    for (int index = 2; index < 7; ++index) {
+        popped_value = queue.pop();
+        EXPECT_EQ(*popped_value, index);
+    }
+    for (int index = 10; index < 20; ++index) {
+        queue.push(index);
+    }
+    for (int index = 7; index < 20; ++index) {
+        popped_value = queue.pop();
+        EXPECT_EQ(*popped_value, index);
+    }
+
+    EXPECT_TRUE(queue.empty());
 }
 
 TEST(simple_queue_tests, emplace_tests)
 {
-    SimpleQueue<std::pair<int, double>> sq;
+    SimpleQueue<std::pair<int, double>> queue;
 
-    sq.emplace(10, 45.4);
-    sq.emplace(11, 34.1);
-    sq.emplace(12, 34.2);
+    queue.emplace(10, 45.4);
+    queue.emplace(11, 34.1);
+    queue.emplace(12, 34.2);
 
-    EXPECT_EQ(sq.size(), 3);
-    auto b = sq.pop();
-    EXPECT_EQ(b->first, 10);
-    EXPECT_EQ(b->second, 45.4);
-    b = sq.pop();
-    EXPECT_EQ(b->first, 11);
-    EXPECT_EQ(b->second, 34.1);
+    EXPECT_EQ(queue.size(), 3);
+    auto popped_value = queue.pop();
+    EXPECT_EQ(popped_value->first, 10);
+    EXPECT_EQ(popped_value->second, 45.4);
+    popped_value = queue.pop();
+    EXPECT_EQ(popped_value->first, 11);
+    EXPECT_EQ(popped_value->second, 34.1);
 }
 
 /** test with single consumer/single producer*/
 TEST(simple_queue_tests, multithreaded_tests)
 {
-    SimpleQueue<int64_t> sq(1010000);
+    SimpleQueue<int64_t> queue(1010000);
 
-    for (int64_t ii = 0; ii < 10'000; ++ii) {
-        sq.push(ii);
+    for (int64_t index = 0; index < 10'000; ++index) {
+        queue.push(index);
     }
-    auto prod1 = [&]() {
-        for (int64_t ii = 10'000; ii < 1'010'000; ++ii) {
-            sq.push(ii);
+    auto producer = [&]() {
+        for (int64_t index = 10'000; index < 1'010'000; ++index) {
+            queue.push(index);
         }
     };
 
-    auto cons = [&]() {
-        auto res = sq.pop();
-        int64_t cnt = 1;
-        while ((res)) {
-            auto nres = sq.pop();
-            if (!nres) {  // make an additional sleep period so the producer can
-                          // catch up
+    auto consumer = [&]() {
+        auto result = queue.pop();
+        int64_t count = 1;
+        while (result) {
+            auto next_result = queue.pop();
+            if (!next_result) {  // make an additional sleep period so the producer can
+                                 // catch up
                 std::this_thread::sleep_for(std::chrono::milliseconds(100));
-                nres = sq.pop();
+                next_result = queue.pop();
             }
-            if (nres) {
-                if (*nres > *res) {
-                    ++cnt;
-                } else {
-                    if (*nres > 0) {
-                        printf(
-                            "%d came before %d\n",
-                            static_cast<int>(*nres),
-                            static_cast<int>(*res));
-                    }
+            if (next_result) {
+                if (*next_result > *result) {
+                    ++count;
+                } else if (*next_result > 0) {
+                    std::cout << *next_result << " came before " << *result
+                              << '\n';
                 }
             }
-            res = nres;
+            result = next_result;
         }
-        return cnt;
+        return count;
     };
 
-    auto ret = std::async(std::launch::async, prod1);
+    auto producer_task = std::async(std::launch::async, producer);
+    auto consumer_task = std::async(std::launch::async, consumer);
 
-    auto res = std::async(std::launch::async, cons);
-
-    ret.wait();
-    auto consumed_count = res.get();
+    producer_task.wait();
+    auto consumed_count = consumer_task.get();
     EXPECT_EQ(consumed_count, 1'010'000);
 }
 
 /** test with multiple consumer/single producer*/
 TEST(simple_queue_tests, multithreaded_tests2)
 {
-    SimpleQueue<int64_t> sq(1010000);
+    SimpleQueue<int64_t> queue(1010000);
 
-    for (int64_t ii = 0; ii < 10'000; ++ii) {
-        sq.push(ii);
+    for (int64_t index = 0; index < 10'000; ++index) {
+        queue.push(index);
     }
-    auto prod1 = [&]() {
-        for (int64_t ii = 10'000; ii < 2'010'000; ++ii) {
-            sq.push(ii);
+    auto producer = [&]() {
+        for (int64_t index = 10'000; index < 2'010'000; ++index) {
+            queue.push(index);
         }
     };
 
-    auto cons = [&]() {
-        auto res = sq.pop();
-        int64_t cnt = 0;
-        while ((res)) {
-            ++cnt;
-            res = sq.pop();
-            if (!res) {  // make an additional sleep period so the producer can
-                         // catch up
+    auto consumer = [&]() {
+        auto result = queue.pop();
+        int64_t count = 0;
+        while (result) {
+            ++count;
+            result = queue.pop();
+            if (!result) {  // make an additional sleep period so the producer can
+                            // catch up
                 std::this_thread::sleep_for(std::chrono::milliseconds(100));
-                res = sq.pop();
+                result = queue.pop();
             }
         }
-        return cnt;
+        return count;
     };
 
-    auto ret = std::async(std::launch::async, prod1);
+    auto producer_task = std::async(std::launch::async, producer);
 
-    auto res1 = std::async(std::launch::async, cons);
-    auto res2 = std::async(std::launch::async, cons);
-    auto res3 = std::async(std::launch::async, cons);
-    ret.wait();
-    auto first_count = res1.get();
-    auto second_count = res2.get();
-    auto third_count = res3.get();
+    auto first_consumer_task = std::async(std::launch::async, consumer);
+    auto second_consumer_task = std::async(std::launch::async, consumer);
+    auto third_consumer_task = std::async(std::launch::async, consumer);
+    producer_task.wait();
+    auto first_count = first_consumer_task.get();
+    auto second_count = second_consumer_task.get();
+    auto third_count = third_consumer_task.get();
 
     EXPECT_EQ(first_count + second_count + third_count, 2'010'000);
 }
@@ -199,45 +196,45 @@ TEST(simple_queue_tests, multithreaded_tests2)
 /** test with multiple producer/multiple consumer*/
 TEST(simple_queue_tests, multithreaded_tests3)
 {
-    SimpleQueue<int64_t> sq;
-    sq.reserve(3'010'000);
-    for (int64_t ii = 0; ii < 10'000; ++ii) {
-        sq.push(ii);
+    SimpleQueue<int64_t> queue;
+    queue.reserve(3'010'000);
+    for (int64_t index = 0; index < 10'000; ++index) {
+        queue.push(index);
     }
-    auto prod1 = [&]() {
-        for (int64_t ii = 0; ii < 1'000'000; ++ii) {
-            sq.push(ii);
+    auto producer = [&]() {
+        for (int64_t index = 0; index < 1'000'000; ++index) {
+            queue.push(index);
         }
     };
 
-    auto cons = [&]() {
-        auto res = sq.pop();
-        int64_t cnt = 0;
-        while ((res)) {
-            ++cnt;
-            res = sq.pop();
-            if (!res) {  // make an additional sleep period so the producer can
-                         // catch up
+    auto consumer = [&]() {
+        auto result = queue.pop();
+        int64_t count = 0;
+        while (result) {
+            ++count;
+            result = queue.pop();
+            if (!result) {  // make an additional sleep period so the producer can
+                            // catch up
                 std::this_thread::sleep_for(std::chrono::milliseconds(100));
-                res = sq.pop();
+                result = queue.pop();
             }
         }
-        return cnt;
+        return count;
     };
 
-    auto ret1 = std::async(std::launch::async, prod1);
-    auto ret2 = std::async(std::launch::async, prod1);
-    auto ret3 = std::async(std::launch::async, prod1);
+    auto first_producer_task = std::async(std::launch::async, producer);
+    auto second_producer_task = std::async(std::launch::async, producer);
+    auto third_producer_task = std::async(std::launch::async, producer);
 
-    auto res1 = std::async(std::launch::async, cons);
-    auto res2 = std::async(std::launch::async, cons);
-    auto res3 = std::async(std::launch::async, cons);
-    ret1.wait();
-    ret2.wait();
-    ret3.wait();
-    auto first_count = res1.get();
-    auto second_count = res2.get();
-    auto third_count = res3.get();
+    auto first_consumer_task = std::async(std::launch::async, consumer);
+    auto second_consumer_task = std::async(std::launch::async, consumer);
+    auto third_consumer_task = std::async(std::launch::async, consumer);
+    first_producer_task.wait();
+    second_producer_task.wait();
+    third_producer_task.wait();
+    auto first_count = first_consumer_task.get();
+    auto second_count = second_consumer_task.get();
+    auto third_count = third_consumer_task.get();
 
     EXPECT_EQ(first_count + second_count + third_count, 3'010'000);
 }
@@ -245,55 +242,55 @@ TEST(simple_queue_tests, multithreaded_tests3)
 /** test with multiple producer/multiple consumer*/
 TEST(simple_queue_tests, move_construct)
 {
-    SimpleQueue<int64_t> sq;
-    sq.push(54);
-    sq.push(55);
-    SimpleQueue<int64_t> sq2(std::move(sq));
+    SimpleQueue<int64_t> queue;
+    queue.push(54);
+    queue.push(55);
+    SimpleQueue<int64_t> moved_queue(std::move(queue));
 
-    auto res = sq2.pop();
-    EXPECT_EQ(*res, 54);
-    res = sq2.pop();
-    EXPECT_EQ(*res, 55);
-    res = sq2.pop();
-    EXPECT_FALSE(res);
+    auto result = moved_queue.pop();
+    EXPECT_EQ(*result, 54);
+    result = moved_queue.pop();
+    EXPECT_EQ(*result, 55);
+    result = moved_queue.pop();
+    EXPECT_FALSE(result);
 }
 
 /** test with multiple producer/multiple consumer*/
 TEST(simple_queue_tests, move_assign)
 {
-    SimpleQueue<int64_t> sq;
-    sq.push(54);
-    sq.push(55);
-    SimpleQueue<int64_t> sq2;
-    sq2 = std::move(sq);
+    SimpleQueue<int64_t> queue;
+    queue.push(54);
+    queue.push(55);
+    SimpleQueue<int64_t> target_queue;
+    target_queue = std::move(queue);
 
-    auto res2 = sq2.peek();
-    EXPECT_EQ(*res2, 54);
-    auto res = sq2.pop();
-    EXPECT_EQ(*res, 54);
+    auto peeked_result = target_queue.peek();
+    EXPECT_EQ(*peeked_result, 54);
+    auto result = target_queue.pop();
+    EXPECT_EQ(*result, 54);
 
-    res = sq2.pop();
-    EXPECT_EQ(*res, 55);
-    res = sq2.pop();
-    EXPECT_FALSE(res);
-    res2 = sq2.peek();
-    EXPECT_FALSE(res2);
+    result = target_queue.pop();
+    EXPECT_EQ(*result, 55);
+    result = target_queue.pop();
+    EXPECT_FALSE(result);
+    peeked_result = target_queue.peek();
+    EXPECT_FALSE(peeked_result);
 }
 
 /** test with multiple producer/multiple consumer*/
 TEST(simple_queue_tests, pop_test)
 {
-    SimpleQueue<int64_t> sq;
-    sq.push(54);
-    sq.push(55);
-    sq.push(56);
-    sq.pushVector(std::vector<int64_t>{57, 58, 59});
-    sq.pop();
-    sq.pop();
-    sq.push(60);
-    EXPECT_TRUE(sq.pop());
-    EXPECT_TRUE(sq.pop());
-    EXPECT_TRUE(sq.pop());
-    EXPECT_TRUE(sq.pop());
-    EXPECT_TRUE(sq.pop());
+    SimpleQueue<int64_t> queue;
+    queue.push(54);
+    queue.push(55);
+    queue.push(56);
+    queue.pushVector(std::vector<int64_t>{57, 58, 59});
+    queue.pop();
+    queue.pop();
+    queue.push(60);
+    EXPECT_TRUE(queue.pop());
+    EXPECT_TRUE(queue.pop());
+    EXPECT_TRUE(queue.pop());
+    EXPECT_TRUE(queue.pop());
+    EXPECT_TRUE(queue.pop());
 }
